9e99f6097d2513bda30b1bc532fe31946169b4ad
Alexander Arjona <alexander.arjona@g.austincc.edu>
2025-09-27 22:02:37 -0500

    Update decisions.py

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,17 +1,13 @@
 def get_options_ratio(option, total):
-    if total == 0:
-        return 0  # avoid division by zero
-    return option / total
-
+    return option / total if total != 0 else 0
 
 def get_faculty_rating(ratio):
-    if ratio >= 0.9 and ratio < 1:
+    if 0.9 <= ratio < 1:
         return "Excellent"
-    elif ratio > 0.8:
+    if ratio > 0.8:
         return "Very Good"
-    elif ratio > 0.7:
+    if ratio > 0.7:
         return "Good"
-    elif ratio > 0.6:
+    if ratio > 0.6:
         return "Needs Improvement"
-    else:
-        return "Unacceptable"
+    return "Unacceptable"
