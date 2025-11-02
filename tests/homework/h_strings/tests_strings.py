5ba52d5fb9b56f3576e09c1500943261f80f3e01
Alexander Arjona <alexander.arjona@g.austincc.edu>
2025-11-01 23:12:27 -0500

    Update tests_strings.py

--- tests/homework/h_strings/tests_strings.py
+++ tests/homework/h_strings/tests_strings.py
@@ -1,1 +1,13 @@
-#
+#import unittest
+from src.homework.h_strings.strings import get_hamming_distance, get_dna_complement
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_hamming_distance(self):
+        self.assertEqual(
+            get_hamming_distance("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT"),
+            7
+        )
+
+    def test_get_dna_complement(self):
+        self.assertEqual(get_dna_complement("AAAACCCGGT"), "ACCGGGTTTT")
