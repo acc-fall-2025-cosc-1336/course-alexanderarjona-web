4648a1bc8b48515bbafcf175617a2ce67190a8e5
Alexander Arjona <alexander.arjona@g.austincc.edu>
2025-11-01 23:04:23 -0500

    Update strings.py

--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,7 @@
-#
+def get_hamming_distance(dna1: str, dna2: str) -> int:
+    # Return the Hamming distance between dna1 and dna2
+    pass
+
+def get_dna_complement(dna: str) -> str:
+    # Return the reverse complement of dna
+    pass#
